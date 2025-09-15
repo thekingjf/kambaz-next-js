aaf8010e86faec7d72a10100d6355bd009e404a3
thekingjf <vittiniruiz.j@northeastern.edu>
2025-09-14 20:00:05 -0400

    First commit

---
+++ app/Labs/Lab1/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+
+
+export default function Lab1() {
+  return (
+    <div id="wd-lab1">
+      <h2>Lab 1</h2>
+    </div>
+  );
+}
