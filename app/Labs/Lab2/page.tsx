74f93584b21ced528f4ff3a454606b063bf71aa3
thekingjf <vittiniruiz.j@northeastern.edu>
2025-09-15 04:44:20 -0400

    Finished Lab 1

---
+++ app/Labs/Lab2/page.tsx
@@ -0,0 +1,8 @@
+function Lab2() {
+ return (
+  <div>
+   <h2>Lab 2</h2>
+  </div>
+ );
+}
+export default Lab2;
---
+++ app/Labs/Lab3/page.tsx
@@ -0,0 +1,8 @@
+function Lab3() {
+ return (
+  <div>
+   <h2>Lab 3</h2>
+  </div>
+ );
+}
+export default Lab3;
