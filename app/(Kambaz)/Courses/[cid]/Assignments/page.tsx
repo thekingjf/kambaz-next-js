3eb31b8ac0e7c2de1b20686b0c3444c83d82c329
thekingjf <vittiniruiz.j@northeastern.edu>
2025-09-19 21:44:14 -0400

    Fixed assignment linking

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,37 +1,49 @@
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
-      <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+      <input
+        placeholder="Search for Assignments"
+        id="wd-search-assignment"
+      />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
       <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button> </h3>
+        ASSIGNMENTS 40% of Total <button>+</button>{" "}
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-            <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
-                A1 - ENV + HTML <br />
-            </a>
-                Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am |
-                <strong> Due </strong>May 13 at 11:59pm | 100 pts 
+          <Link
+            href="/Courses/1234/Assignments/123"
+            className="wd-assignment-link"
+          >
+            A1 - ENV + HTML <br />
+          </Link>
+          Multiple Modules | <strong>Not available until</strong> May 6 at
+          12:00am | <strong> Due </strong>May 13 at 11:59pm | 100 pts
         </li>
         <li className="wd-assignment-list-item">
-            <a href="/Courses/1234/Assignments/124"
-                className="wd-assignment-link" >
-                A2 - CSS + BOOTSTRAP <br />
-            </a>
-                Multiple Modules | <strong>Not available until</strong> May 13 at 12:00am |
-                <strong> Due </strong>May 20 at 11:59pm | 100 pts
-        </li> 
-                <li className="wd-assignment-list-item">
-            <a href="/Courses/1234/Assignments/125"
-                className="wd-assignment-link" >
-                A3 - JAVASCRIPT + REACT <br />
-            </a>
-                Multiple Modules | <strong>Not available until</strong> May 20 at 12:00am |
-                <strong> Due </strong>May 27 at 11:59pm | 100 pts
-        </li> 
+          <Link
+            href="/Courses/1234/Assignments/124"
+            className="wd-assignment-link"
+          >
+            A2 - CSS + BOOTSTRAP <br />
+          </Link>
+          Multiple Modules | <strong>Not available until</strong> May 13 at
+          12:00am | <strong> Due </strong>May 20 at 11:59pm | 100 pts
+        </li>
+        <li className="wd-assignment-list-item">
+          <Link
+            href="/Courses/1234/Assignments/125"
+            className="wd-assignment-link"
+          >
+            A3 - JAVASCRIPT + REACT <br />
+          </Link>
+          Multiple Modules | <strong>Not available until</strong> May 20 at
+          12:00am | <strong> Due </strong>May 27 at 11:59pm | 100 pts
+        </li>
       </ul>
     </div>
-);}
+  );
+}
