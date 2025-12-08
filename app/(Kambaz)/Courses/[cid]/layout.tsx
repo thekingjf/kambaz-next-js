b59412d311271fad561f9a2d2cafe5f3eb3e9044
thekingjf <vittiniruiz.j@northeastern.edu>
2025-12-07 22:29:13 -0500

    Fixed courses

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -4,12 +4,19 @@ import { useParams } from "next/navigation";
 import { ReactNode } from "react";
 import { RootState } from "../../store";
 
+type Course = {
+  _id: string;
+  name: string;
+};
+
 export default function CoursesLayout({ children }: { children: ReactNode }) {
-  const { cid } = useParams();
+  const { cid } = useParams<{ cid: string }>();
+
   const { courses: courseList } = useSelector(
     (state: RootState) => state.coursesReducer
-  );
-  const course = courseList.find((c: any) => c._id === cid);
+  ) as { courses: Course[] };
+
+  const course = courseList.find((c) => c._id === cid);
 
   return (
     <div id="wd-courses">
